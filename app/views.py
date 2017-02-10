c69b1f9dfd64d878b8c4e146c9911f3e295784dd
Karmanno <zenbudd@mail.ru>
2017-02-10 07:45:58 +0300

    Go to the third lesson

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    user = {'nickname': 'Dimas'}
+    return render_template('index.html', title='Home', posts=posts, user=user)
